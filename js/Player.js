f088e9c4fa0ae755d22a351f2763f1225d04488d
annika01 <71920315+annika01@users.noreply.github.com>
2020-12-10 17:27:24 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,30 @@
+class Player{
+    constructor(){
+        this.index= null
+        this.distance=0
+        this.name=null
+    }
+    getCount(){
+        db.ref("playerCount").on("value", function(data){
+            playerCount=data.val()
+        })
+    }
+    
+    updateCount(count){
+        db.ref("/").update({
+           playerCount: count
+        })
+    }
+    update(){
+        var playerIndex = "players/player"+this.index
+        db.ref(playerIndex).set({ 
+           name:this.name,
+           distance:this.distance
+        })
+    }
+    static getPlayerInfo(){
+        db.ref("players").on("value", (data)=>{
+allPlayers=data.val()
+console.log(allPlayers)
+        })
+    }}
